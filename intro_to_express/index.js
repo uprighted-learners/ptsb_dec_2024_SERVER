c30f597a11bb7d954056e49807951a50dce4ff0b
u4ik <insighteuphoric@gmail.com>
2025-02-19 20:22:41 -0500

    added post man requests

--- intro_to_express/index.js
+++ intro_to_express/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 // To access, send a GET request to localhost:3000
 app.get("/hello", (request, response) => {
-  response.send("Hello world");
+  response.status(200).json({status: "Hello world"});
 });
 
 app.get("/date", (req, res) => {
@@ -74,6 +74,21 @@ app.get("/api/users/:userId", (req, res) => {
   res.json(myFilteredArray[0])
 
 });
+
+app.post("/create-user", (req, res)=> {
+
+  console.log("THE BODY: ", req.body);
+
+  let userName = req.body.username
+  let password = req.body.password
+
+  // Write the user data to a database here ... 
+  res.json({
+    status: `${userName} was created!`
+  })
+  
+})
+
 
 // Listens for connections to our port
 app.listen(port, () => {
